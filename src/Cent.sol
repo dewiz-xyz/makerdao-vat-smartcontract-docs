21aa725026dc869b8a19e82e9af51a5b306d6387
0xcentur1on <marcusaureliuscenturion@gmail.com>
2023-08-15 19:05:51 -0300

    refactor: remove mint in constructor

--- src/Cent.sol
+++ src/Cent.sol
@@ -4,10 +4,5 @@ pragma solidity 0.8.19;
 import {Dai} from "./dai.sol";
 
 contract CenturionDai is Dai {
-    constructor() Dai(1337) {
-        uint256 balance = 1000 * uint256(10**decimals);
-        balanceOf[msg.sender] = add(balanceOf[msg.sender], balance);
-        totalSupply = add(totalSupply, balance);
-        emit Transfer(address(0), msg.sender, balance);
-    }
+    constructor() Dai(1337) {}
 }
