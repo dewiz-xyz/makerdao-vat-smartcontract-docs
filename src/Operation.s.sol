c121b03a22b5f9521865a36bde823069c46135a5
0xcentur1on <marcusaureliuscenturion@gmail.com>
2023-09-08 17:57:58 -0300

    refactor: remove borrow and payback contracts

--- src/Operation.s.sol
+++ src/Operation.s.sol
@@ -9,102 +9,6 @@ import {Vat} from "dss/vat.sol";
 import {Dai} from "dss/dai.sol";
 import {GemJoin, DaiJoin} from "dss/join.sol";
 
-//  ./scripts/forge-script.sh ./src/Operation.s.sol:Borrow --fork-url=$RPC_URL --broadcast -vvvv
-contract Borrow is Script {
-    Registry public registry;
-    Vat public vat;
-    Dai public dai;
-    Denarius public denarius;
-    GemJoin public gemJoin;
-    DaiJoin public daiJoin;
-
-    function run() external {
-        uint256 valueToLock = 12 * 10**18;
-        uint256 valueToDrawInDai = 5 * 10**18;
-
-        vm.startBroadcast();
-
-        (bool success, address registryAddress) = RegistryUtil.getRegistryAddress();
-        if (!success) {
-            console2.log("Error creating new Registry instance!");
-            revert();
-        }
-        registry = Registry(registryAddress);
-        gemJoin = GemJoin(registry.lookUp("GemJoin"));
-        vat = Vat(registry.lookUp("SampleVat"));
-        daiJoin = DaiJoin(registry.lookUp("DaiJoin"));
-        dai = Dai(registry.lookUp("CenturionDai"));
-
-        console2.log("Before - I am %s and my balance in Dai: %s", msg.sender, dai.balanceOf(msg.sender));
-
-        gemJoin.join(msg.sender, valueToLock);
-
-        (, uint256 rate, , , ) = vat.ilks("Denarius-A");
-        uint256 dart = Numbers.divup(Numbers.mul(Numbers.ray(), valueToDrawInDai), rate);
-        require(dart <= 2**255 - 1, "RwaUrn/overflow");
-        uint256 dink = dart * 2;
-
-        vat.frob(
-            "Denarius-A", // ilk
-            msg.sender,
-            msg.sender,
-            msg.sender, // To keep it simple, use your address for both `u`, `v` and `w`
-            int256(dink), // with 10**18 precision
-            int256(dart) // with 10**18 precision
-        );
-
-        daiJoin.exit(msg.sender, valueToDrawInDai);
-
-        console2.log("dink: %d - dart: %d", dink, dart);
-        console2.log("After - I am %s and my balance in Dai: %s", msg.sender, dai.balanceOf(msg.sender));
-
-        vm.stopBroadcast();
-    }
-}
-
-// ./scripts/forge-script.sh ./src/Operation.s.sol:PayBack --fork-url=$RPC_URL --broadcast -vvvv
-contract PayBack is Script {
-    uint256 internal constant _VALUE_TO_PAYBACK = 2 * 10**18;
-
-    function run() external {
-        vm.startBroadcast();
-
-        (bool success, address registryAddress) = RegistryUtil.getRegistryAddress();
-        if (!success) {
-            console2.log("Error creating new Registry instance!");
-            revert();
-        }
-        Registry registry = Registry(registryAddress);
-        Vat vat = Vat(registry.lookUp("Vat"));
-        Dai dai = Dai(registry.lookUp("Dai"));
-        Denarius denarius = Denarius(registry.lookUp("Denarius"));
-        DaiJoin daiJoin = DaiJoin(registry.lookUp("DaiJoin"));
-        GemJoin gemJoin = GemJoin(registry.lookUp("GemJoin"));
-
-        daiJoin.join(msg.sender, _VALUE_TO_PAYBACK);
-        (, uint256 rate, , , ) = vat.ilks("Denarius-A");
-        uint256 dart = Numbers.mul(Numbers.ray(), _VALUE_TO_PAYBACK) / rate;
-        uint256 dink = dart * 2;
-        require(dart <= 2**255 && dart <= 2**255, "RwaUrn/overflow");
-
-        vat.frob("Denarius-A", msg.sender, msg.sender, msg.sender, -1, -1);
-
-        gemJoin.exit(msg.sender, _VALUE_TO_PAYBACK);
-
-        console2.log("dink: %d - dart: %d", dink, dart);
-
-        uint256 cBalance = dai.balanceOf(msg.sender);
-        uint256 cEtherFormat = cBalance / (1 * 10**18);
-        console2.log("Dai balance: %d - %d", cEtherFormat, cBalance);
-
-        uint256 dBalance = denarius.balanceOf(msg.sender);
-        uint256 dEtherFormat = dBalance / (1 * 10**18);
-        console2.log("denarius balance: %d - %d", dEtherFormat, dBalance);
-
-        vm.stopBroadcast();
-    }
-}
-
 // ./scripts/forge-script.sh ./src/Operation.s.sol:InfoBalances --fork-url=$RPC_URL --broadcast -vvvv
 contract InfoBalances is Script {
     function run() external {
@@ -128,12 +32,25 @@ contract InfoBalances is Script {
         uint256 dEtherFormat = dBalance / (1 * 10**18);
         console2.log("denarius balance: %d - %d", dEtherFormat, dBalance);
 
-        (uint256 ink, uint256 art) = vat.urns("Denarius-A", msg.sender);
-        console2.log("vat urn - ink: %d - art: %d", ink, art);
+        (uint256 inkA, uint256 artA) = vat.urns("Denarius-A", msg.sender);
+        console2.log("Denarius-A - vat urn - ink: %d - art: %d", inkA, artA);
+
+        (uint256 inkB, uint256 artB) = vat.urns("Denarius-B", msg.sender);
+        console2.log("Denarius-B - vat urn - ink: %d - art: %d", inkB, artB);
 
         // solhint-disable-next-line
-        (uint256 Art, , , , ) = vat.ilks("Denarius-A");
-        console2.log("Total Denarius-A ART: %d", Art);
+        (uint256 ArtA, , , , ) = vat.ilks("Denarius-A");
+        console2.log("Total Denarius-A ART: %d", ArtA);
+
+        // solhint-disable-next-line
+        (uint256 ArtB, , , , ) = vat.ilks("Denarius-B");
+        console2.log("Total Denarius-B ART: %d", ArtB);
+
+        uint256 gemV = vat.gem("Denarius-A", msg.sender);
+        console2.log("gemV Denarius-A: %d", gemV);
+
+        gemV = vat.gem("Denarius-B", msg.sender);
+        console2.log("gemV Denarius-B: %d", gemV);
 
         vm.stopBroadcast();
     }
